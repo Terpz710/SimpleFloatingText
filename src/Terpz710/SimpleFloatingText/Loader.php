ba0dae958966bb6380fe1478258c95e08c63feeb
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-04-19 06:50:11 -0700

    Error fixes

--- src/Terpz710/SimpleFloatingText/Loader.php
+++ src/Terpz710/SimpleFloatingText/Loader.php
@@ -19,23 +19,40 @@ use Terpz710\SimpleFloatingText\Command\FloatingTextCommand;
 
 class Loader extends PluginBase implements Listener {
 
+    private static $instance;
+
+    public function onLoad(): void {
+        self::$instance = $this;
+    }
+
+
     public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
 
-        $ftFolderPath = $this->getDataFolder() . "FloatingText";
-        if (!is_dir($ftFolderPath)) {
-            @mkdir($ftFolderPath);
-        }
-
-        $config = new Config($ftFolderPath . DIRECTORY_SEPARATOR . "floating_text.json", Config::JSON);
+        $config = new Config($this->getDataFolder() . "floating_text.json", Config::JSON);
 
         $this->getServer()->getCommandMap()->register("ft", new FloatingTextCommand($this, $ftFolderPath));
     }
 
     public function onDisable(): void {
-        $ftFolderPath = $this->getDataFolder() . "FloatingText";
-        $config = new Config($ftFolderPath . DIRECTORY_SEPARATOR . "floating_text.json", Config::JSON);
-        FloatingTextAPI::saveToFile($ftFolderPath);
+        FloatingKDRAPI::saveFile();
+    }
+
+    public static function getInstance(): ?Main {
+        return self::$instance;
+    }
+
+    public function onChunkLoad(ChunkLoadEvent $event): void {
+        $filePath = $this->getDataFolder() . "floating_text_data.json";
+        FloatingKDRAPI::loadFromFile($filePath);
+    }
+
+    public function onChunkUnload(ChunkUnloadEvent $event): void {
+        FloatingKDRAPI::saveFile();
+    }
+
+    public function onWorldUnload(WorldUnloadEvent $event): void {
+        FloatingKDRAPI::saveFile();
     }
 
     public function onEntityTeleport(EntityTeleportEvent $event): void {
@@ -43,24 +60,14 @@ class Loader extends PluginBase implements Listener {
         if ($entity instanceof Player) {
             $fromWorld = $event->getFrom()->getWorld();
             $toWorld = $event->getTo()->getWorld();
-            
+        
             if ($fromWorld !== $toWorld) {
-                foreach (FloatingTextAPI::$floatingText as $tag => [$position, $floatingText]) {
+                foreach (FloatingKDRAPI::$floatingText as $tag => [$position, $floatingText]) {
                     if ($position->getWorld() === $fromWorld) {
-                        FloatingTextAPI::makeInvisible($tag);
+                        FloatingKDRAPI::makeInvisible($tag);
                     }
                 }
             }
         }
-    }
-
-    public function onChunkLoad(ChunkLoadEvent $event): void {
-        $ftFolderPath = $this->getDataFolder() . "FloatingText";
-        FloatingTextAPI::loadFromFile($ftFolderPath . DIRECTORY_SEPARATOR . "floating_text.json", $ftFolderPath);
-    }
-
-    public function onWorldUnload(WorldUnloadEvent $event): void {
-        $ftFolderPath = $this->getDataFolder() . 'FT'; 
-        FloatingTextAPI::saveToFile($ftFolderPath);
     }
 }
