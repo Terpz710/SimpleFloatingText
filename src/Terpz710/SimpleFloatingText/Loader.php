ab8a5e8e8d2e38a9b872515966c1ef5e6239bd86
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-05-20 11:09:51 -0700

    task

--- src/Terpz710/SimpleFloatingText/Loader.php
+++ src/Terpz710/SimpleFloatingText/Loader.php
@@ -6,10 +6,12 @@ namespace Terpz710\SimpleFloatingText;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+use pocketmine\scheduler\TaskScheduler;
 
 use Terpz710\SimpleFloatingText\API\FloatingTextAPI;
 use Terpz710\SimpleFloatingText\Command\FloatingTextCommand;
 use Terpz710\SimpleFloatingText\Event\WorldManagement;
+use Terpz710\SimpleFloatingText\Task\UpdateTask;
 
 class Loader extends PluginBase {
 
@@ -23,6 +25,7 @@ class Loader extends PluginBase {
         $this->getServer()->getPluginManager()->registerEvents(new WorldManagement($this), $this);
         $config = new Config($this->getDataFolder() . "floating_text.json", Config::JSON);
         $this->getServer()->getCommandMap()->register("ft", new FloatingTextCommand($this));
+        $this->getScheduler()->scheduleRepeatingTask(new UpdateTask(), 20);
     }
 
     public function onDisable(): void {
