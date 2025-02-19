456c3458ec2e72d1da1b017fd64a0573298d02be
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-02-19 03:35:26 -0800

    Update FloatingTextCommand.php

--- src/terpz710/simplefloatingtext/command/FloatingTextCommand.php
+++ src/terpz710/simplefloatingtext/command/FloatingTextCommand.php
@@ -4,120 +4,23 @@ declare(strict_types=1);
 
 namespace terpz710\simplefloatingtext\command;
 
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-
-use pocketmine\player\Player;
-
-use pocketmine\plugin\Plugin;
-use pocketmine\plugin\PluginOwned;
-
-use pocketmine\world\Position;
-
 use terpz710\simplefloatingtext\Loader;
-use terpz710\simplefloatingtext\api\FloatingText;
 
-class FloatingTextCommand extends Command implements PluginOwned {
+use terpz710\simplefloatingtext\command\sub\CreateSubCommand;
+use terpz710\simplefloatingtext\command\sub\RemoveSubCommand;
+use terpz710\simplefloatingtext\command\sub\EditSubCommand;
+use terpz710\simplefloatingtext\command\sub\MoveSubCommand;
 
-    public function __construct() {
-        parent::__construct("ft", "Manage Floating Texts", ["floatingtext"]);
+use CortexPE\Commando\BaseCommand;
+
+class FloatingTextCommand extends BaseCommand {
+
+    protected function prepare() : void{
         $this->setPermission("simplefloatingtext.cmd");
         
-        $this->plugin = Loader::getInstance();
-    }
-
-    public function execute(CommandSender $sender, string $commandLabel, array $args) : bool{
-        if (!$this->testPermission($sender)) {
-            return false;
-        }
-        
-        if ($sender instanceof Player) {
-            if (isset($args[0])) {
-                switch ($args[0]) {
-                    case "create":
-                        if (isset($args[1]) && isset($args[2])) {
-                            $tag = (string) $args[1];
-                            if (array_key_exists($tag, FloatingText::$floatingText)) {
-                                $sender->sendMessage("§l§f(§c!§f)§r§f A floating text with the tag §e{$tag}§f already exists!");
-                                break;
-                            }
-                            $position = $sender->getPosition();
-                            $text = implode(" ", array_slice($args, 2));
-                            $text = str_replace("\n", "{line}", $text);
-                            FloatingText::create($position, $tag, $text);
-                            $sender->sendMessage("§l§f(§a!§f)§r§f Floating text created with the tag §e{$tag}§f and text: {$text}");
-                        } else {
-                            $sender->sendMessage("Usage: §e/ft create <tag> <text>");
-                        }
-                        break;
-
-                    case "remove":
-                        if (isset($args[1])) {
-                            $tag = $args[1];
-                            if (array_key_exists($tag, FloatingText::$floatingText)) {
-                                FloatingText::remove($tag);
-                                $sender->sendMessage("§l§f(§e!§f)§r§f Floating text with the tag §e{$tag}§f was removed!");
-                            } else {
-                                $sender->sendMessage("§l§f(§c!§f)§r§f No floating text found with the tag §e{$tag}§f!");
-                            }
-                        } else {
-                            $sender->sendMessage("Usage: §e/ft remove <tag>");
-                        }
-                        break;
-
-                    case "edit":
-                        if (isset($args[1]) && isset($args[2])) {
-                            $tag = $args[1];
-                            $text = implode(" ", array_slice($args, 2));
-                            $text = str_replace("\n", "{line}", $text);
-                            if (array_key_exists($tag, FloatingText::$floatingText)) {
-                                FloatingText::update($tag, $text);
-                                $sender->sendMessage("§l§f(§b!§f)§r§f Floating text with the tag §e{$tag}§f has updated the text to: {$text}");
-                            } else {
-                                $sender->sendMessage("§l§f(§c!§f)§r§f No floating text found with the tag §e{$tag}§f!");
-                            }
-                        } else {
-                            $sender->sendMessage("Usage: §e/ft edit <tag> <new-text>");
-                        }
-                        break;
-
-                    case "move":
-                        if (isset($args[1])) {
-                            $tag = $args[1];
-                            $playerPosition = $sender->getPosition();
-                            $x = $playerPosition->x;
-                            $y = $playerPosition->y;
-                            $z = $playerPosition->z;
-
-                            if (array_key_exists($tag, FloatingText::$floatingText)) {
-                                FloatingText::$floatingText[$tag][0]->x = $x;
-                                FloatingText::$floatingText[$tag][0]->y = $y;
-                                FloatingText::$floatingText[$tag][0]->z = $z;
-                                FloatingText::update($tag, FloatingText::$floatingText[$tag][1]->getText());
-
-                                $sender->sendMessage("§l§f(§b!§f)§r§f Floating text with the tag §e{$tag}§f has been moved to your current location!");
-                            } else {
-                                $sender->sendMessage("§l§f(§c!§f)§r§f No floating text found with the tag §e{$tag}§f!");
-                            }
-                        } else {
-                            $sender->sendMessage("Usage: §e/ft move <tag>");
-                        }
-                        break;
-
-                    default:
-                        $sender->sendMessage("Unknown subcommand! Available subcommands: §ecreate§f, §edelete§f, §eedit§f, §eremove§f, §emove§f");
-                        break;
-                }
-            } else {
-                $sender->sendMessage("Usage: §e/ft create|edit|remove|move");
-            }
-        } else {
-            $sender->sendMessage("This command can only be used in-game!");
-        }
-        return true;
-    }
-
-    public function getOwningPlugin() : Plugin{
-        return $this->plugin;
+        $this->registerSubCommand(new CreateSubCommand("create", "Create a floating text"));
+        $this->registerSubCommand(new RemoveSubCommand("remove", "Remove a floating text"));
+        $this->registerSubCommand(new EditSubCommand("edit", "Edit an existing floating text"));
+        $this->registerSubCommand(new MoveSubCommand("move", "Move a floating text to your location"));
     }
 }
