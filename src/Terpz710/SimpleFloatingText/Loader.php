3d976256e7196e6a263f1414482eb7a527d1b81d
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-02-19 02:59:09 -0800

    Update Loader.php

--- src/Terpz710/SimpleFloatingText/Loader.php
+++ src/Terpz710/SimpleFloatingText/Loader.php
@@ -2,37 +2,41 @@
 
 declare(strict_types=1);
 
-namespace Terpz710\SimpleFloatingText;
+namespace terpz710\simplefloatingtext;
 
 use pocketmine\plugin\PluginBase;
+
 use pocketmine\utils\Config;
-use pocketmine\scheduler\TaskScheduler;
 
-use Terpz710\SimpleFloatingText\API\FloatingTextAPI;
-use Terpz710\SimpleFloatingText\Command\FloatingTextCommand;
-use Terpz710\SimpleFloatingText\Event\WorldManagement;
-use Terpz710\SimpleFloatingText\Task\UpdateTask;
+use terpz710\simplefloatingtext\api\FloatingText;
 
-class Loader extends PluginBase {
+use terpz710\simplefloatingtext\command\FloatingTextCommand;
 
-    private static $instance;
+use CortexPE\Commando\PacketHooker;
 
-    public function onLoad(): void {
+final class Loader extends PluginBase {
+
+    protected static self $instance;
+
+    protected function onLoad() : void{
         self::$instance = $this;
     }
 
-    public function onEnable(): void {
+    protected function onEnable() : void{
         $this->getServer()->getPluginManager()->registerEvents(new WorldManagement($this), $this);
-        $config = new Config($this->getDataFolder() . "floating_text.json", Config::JSON);
+
+        if (!PacketHooker::isRegistered()) {
+            PacketHooker::register($this);
+        }
+
         $this->getServer()->getCommandMap()->register("ft", new FloatingTextCommand($this));
-        $this->getScheduler()->scheduleRepeatingTask(new UpdateTask(), 20);
     }
 
-    public function onDisable(): void {
-        FloatingTextAPI::saveFile();
+    public function onDisable() : void{
+        FloatingText::saveFile();
     }
 
-    public static function getInstance(): ?Loader {
+    public static function getInstance() : self{
         return self::$instance;
     }
 }
